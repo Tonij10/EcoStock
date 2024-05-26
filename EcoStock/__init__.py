843dd9119a907573ba781ea1620fb2663209e335
AntonioPaparo <111143902+Tonij10@users.noreply.github.com>
2024-05-26 11:00:49 +0200

    Add files via upload

--- EcoStock/__init__.py
+++ EcoStock/__init__.py
@@ -7,7 +7,7 @@ The functions provided by EcoStock can be used to generate visualizations and in
 """
 
 # The current version of the EcoStock package
-__version__ = "1.2"
+__version__ = "1.3"
 
 # Import the FastAPI application from the api module
 from .api import app
